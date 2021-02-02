4fcf95e475291480299eb8081c5735065d03b2c5
Dianashafee <diana.shafee1997@hotmail.com>
2021-02-02 15:06:20 +0200

    adding home

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,55 @@
 import styles from '../styles.module.css'
 import Image from 'next/image'
+
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <body className={styles.body}>
       <div className={styles.main}>
+        {/* <h1>heeyy</h1> */}
       
       </div>
 
 
-    <div className={styles.hello}>
-      <h1>Welcome to our App</h1>
-      <Link href='/portfolio'>
-            <a>About</a>
-        </Link>
-     
+    <div className={styles.students}>
+      <h1 className={styles.font}>Our Creative Students</h1>
+      <div class="imgsdiv" id="javade">
+            <h2 class="Names"> <a href="">test</a> </h2>
+            <Image
+        src="/dundun.jpg"
+        alt="Picture of the author"
+        width={100}
+        height={100}
+      />
+            <p>any thing</p>
+        </div>
+      
+
+        <div>
+          
+        </div>
+
+    
+      <div class="imgsdiv" id="javade">
+            <h2 class="Names"> <a href="">test</a> </h2>
+            <Image
+        src="/dundun.jpg"
+        alt="Picture of the author"
+        width={100}
+        height={100}
+      />
+            <p>any thing</p>
+        </div>
+      
     </div>
+    
+
+   
+  
     </body>
    
-  )
-}
-
-
+   )
+  }
+  
+  
