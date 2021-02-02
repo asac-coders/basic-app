14295e1e358f855d786144aeec84568eb9fedb8a
Dianashafee <diana.shafee1997@hotmail.com>
2021-02-02 03:22:38 +0200

    adding the basic pages and edit the home page

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import styles from '../styles.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <body className={styles.body}>
+      <div className={styles.main}>
+      
+      </div>
+
+
+    <div className={styles.hello}>
+      <h1>Welcome to our App</h1>
+      <Link href='/portfolio'>
+            <a>About</a>
+        </Link>
+     
+    </div>
+    </body>
+   
+  )
+}
+
+
