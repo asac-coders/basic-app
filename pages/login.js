77825e66b3419b0a699f9d9e0a8bc020982f440b
Dianashafee <diana.shafee1997@hotmail.com>
2021-02-03 15:00:17 +0200

    adding login page

--- pages/login.js
+++ pages/login.js
@@ -37,24 +37,65 @@ const Login = () => {
       });
   }
   return (
+      <div className={styles.loginDiv}>
+
+      
     <form onSubmit={handleSubmit}>
+        <fieldset  className={styles.fieldSet}>
       <p className={styles.login}>Login</p>
-      <input
-        name="email"
-        type="email"
+      <label className={styles.nameLable}>
+          Enter Your Username
+      </label>
+      <input className={styles.username}
+        name="username"
+        type="text"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
+      <br>
+      </br>
+      <br>
+      </br>
+       <label className={styles.nameLable2}>
+          Enter Your password
+      </label>
       <input
         name="password"
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <input type="submit" value="Submit" />
+      
+      <br>
+      </br>
+      <br>
+      </br>
+      <input  type="submit" value="Submit" className={styles.button} />
       {loginError && <p style={{color: 'red'}}>{loginError}</p>}
+      </fieldset>
     </form>
+    </div>
   );
 };
 
 export default Login;
+
+// <Form>
+//   <Form.Group as={Row} controlId="formPlaintextEmail">
+//     <Form.Label column sm="2">
+//       Email
+//     </Form.Label>
+//     <Col sm="10">
+//       <Form.Control plaintext readOnly defaultValue="email@example.com" />
+//     </Col>
+//   </Form.Group>
+
+//   <Form.Group as={Row} controlId="formPlaintextPassword">
+//     <Form.Label column sm="2">
+//       Password
+//     </Form.Label>
+//     <Col sm="10">
+//       <Form.Control type="password" placeholder="Password" />
+//     </Col>
+//   </Form.Group>
+// </Form>
