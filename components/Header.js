79ea25bc5e3ccef5e9a842a12ccbf6b50ea8c55f
Sajanader <sajanader93@gmail.com>
2021-02-05 21:43:15 +0200

    saja

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,10 @@ import { FiLogIn } from "@react-icons/all-files/fi/FiLogIn";
 import { ImHome } from "@react-icons/all-files/im/ImHome";
 import { HiUserGroup } from "@react-icons/all-files/hi/HiUserGroup";
 import { RiLoginBoxFill } from "@react-icons/all-files/ri/RiLoginBoxFill";
+import { GrGithub } from "@react-icons/all-files/gr/GrGithub";
+import { GrTwitter } from "@react-icons/all-files/gr/GrTwitter";
+
+
 
 
 
@@ -53,10 +57,15 @@ export default () => (
             <div className={styles.navlinks}>
 
                 <ul>
-                    <li><a className={styles.li} href="/"> <ImHome className={styles.sa} /></a></li>
-                    <li><a className={styles.li} href="/cards"><HiUserGroup className={styles.sa} /></a></li>
-                    <li><a className={styles.li} href="/login"><RiLoginBoxFill className={styles.sa} /></a></li>
+                    <li><a className={styles.sa}  href="/"> <ImHome className={styles.sa}/></a></li>
+                    <li><a className={styles.sa} href="/cards"><HiUserGroup className={styles.sa} /></a></li>
+                    <li><a  className={styles.sa} href="/login"><RiLoginBoxFill className={styles.sa}/></a></li>
                 </ul>
+            </div>
+            <div className = {styles.rightNav} >
+            <a className={styles.sa} href="https://github.com/asac-coders"><GrGithub className={styles.sa}/> </a>
+            <a className={styles.li} href="#"><GrTwitter className={styles.sa}/> </a>
+        
             </div>
         </nav>
     </header>
